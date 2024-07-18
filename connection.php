65bdcee306ca464362394767050c8474e5c11666
khushjeet <73747101+khushjeet@users.noreply.github.com>
2024-07-18 10:40:28 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+	error_reporting(0);
+$servername = "localhost";
+$username = "ankusame_root";
+$password = "Ismail@203";
+$dbname = "ankusame_purchase_order";
+
+$connectNow = new mysqli($serverHost, $username, $password, $dbname);
+
+  ?>
